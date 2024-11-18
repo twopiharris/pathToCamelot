1b539f932c40f02b898fecb6c874d1503f9d6431
meadams2 <meadams2@bsu.edu>
2024-11-18 10:47:36 -0500

    BasicCharacterClass
    Initializing attributes and defining a testInt function.

--- camelotCharacter.py
+++ camelotCharacter.py
@@ -2,3 +2,17 @@
 CS120
 Path to Camelot
 Character Module"""
+
+class Character(object):
+    def __init__(self, name = "Billy Bob John", hitPoints = 10, hitChance = 30, maxDamage = 3, healingFactor = 20, maxHealing = 4, armor = 0):
+        super().__init__()
+        self.name = name
+        self.hitPoints = hitPoints
+        self.hitChance = hitChance
+        self.maxDamage = maxDamage
+        self.healingFactor = healingFactor
+        self.maxHealing = maxHealing
+        self.armor = armor
+        
+    def testInt(self, value, min = 0, max = 100, default = 0):
+        
