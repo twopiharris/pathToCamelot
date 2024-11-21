2fbba7b8eea2df28860965f9902d89213c2c42c9
meadams2 <meadams2@bsu.edu>
2024-11-21 10:36:44 -0500

    Troubleshooting map

--- camelot.py
+++ camelot.py
@@ -1,8 +1,49 @@
 """Marianne Adams
 CS120
 Path to Camelot"""
-
-map = [
+import pygame, simpleGE
+    
+class Tile(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+#         self.size = (15, 15)
+        self.images = [pygame.image.load("grass.png"),
+                       pygame.image.load("path.png"),
+                       pygame.image.load("water.png"),
+                       pygame.image.load("forest.png")]
+#         self.setSize(32, 32)
+        self.stateName = ["grass", "path", "water", "forest"]
+        self.GRASS = 0
+        self.PATH = 1
+        self.WATER = 2
+        self.FOREST = 3
+        self.state = self.GRASS
+    
+    def setState(self, state):
+        self.state = state
+        self.copyImage(self.images[state])
+    
+class Game(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.setCaption("The Path to Camelot")
+        self.tileset = []
+        
+        self.ROWS = 20
+        self.COLS = 40
+        
+        self.SCREEN_ROWS = 10
+        self.SCREEN_COLS = 10
+        
+        self.offRow = 0
+        self.offCol = 0
+        
+        self.loadMap()
+        
+        self.sprites = [self.tileset]
+        
+    def loadMap(self):
+        map = [
     [1,0,0,0,0,0,0,0,0,0,0,2,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
     [1,1,0,0,0,0,0,0,0,0,0,2,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,1,1,1,1,1,0,0,0,0,0,2,1,0,1,0,3,3,3,3,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -24,3 +65,46 @@ map = [
     [0,2,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,3,3,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0,2,1,3],
     [2,2,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3]
 ]
+        for row in range(self.SCREEN_ROWS):
+            self.tileset.append([])
+            for col in range (self.SCREEN_COLS):
+                currentValue = map[row][col]
+                newTile = Tile(self)
+                newTile.setState(currentValue)
+                xPos = 16 + (32 * col)
+                yPos = 16 + (32 * row)
+                newTile.x = xPos
+                newTile.y = yPos
+                self.tileset[row].append(newTile)
+    
+    def showMap(self):
+        for row in range(self.SCREEN_ROWS):
+            for col in range(self.SCREEN_COLS):
+                currentVal = self.map[row + self.offRow][col + self.offCol]
+                self.tileset[row][col].setState(currentVal)
+                
+    def process(self):
+        if self.isKeyPressed(pygame.K_LEFT):
+            if self.offCol > 0:
+                self.offCol -= 1
+                
+        if self.isKeyPressed(pygame.K_RIGHT):
+            if self.offCol < (self.COLS - self.SCREEN_COLS):
+                self.offCol += 1
+        
+        if self.isKeyPressed(pygame.K_UP):
+            if self.offRow > 0:
+                self.offRow -= 1
+        
+        if self.isKeyPressed(pygame.K_DOWN):
+            if self.offRow < (self.ROWS - self.SCREEN_ROWS):
+                self.offRow += 1
+        
+        self.showMap()
+        
+def main():
+    game = Game()
+    game.start()
+    
+if __name__ == "__main__":
+    main()
